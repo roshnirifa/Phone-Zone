8550c2d5c8a03c133bf0f83407df930610158b7e
roshnirifa <roshnirifa998@gmail.com>
2022-03-01 23:34:33 +0600

    13 pro added

--- js/app.js
+++ js/app.js
@@ -13,7 +13,8 @@ const searchAllPhone = () => {
     // search error
 
     if (searchFieldText == '') {
-        alert('please write something to display')
+        alert('please write something to display');
+
     }
 
     else {
@@ -31,16 +32,13 @@ const searchAllPhone = () => {
             })
     }
 }
-
+// showing the search result
 const displaySerachResult = phones => {
     // console.log(phones);
     for (const phone of phones) {
         // console.log(phone);
-
-
         const searchResult = document.getElementById('search-result');
         const div = document.createElement('div')
-
         div.classList.add('col')
         document.getElementById('spinner').style.display = "none";
         div.innerHTML = `
@@ -51,7 +49,7 @@ const displaySerachResult = phones => {
                         <div class="card-body">
                             <h4> Phone Name : ${phone.phone_name} </h4>
                             <h5 class="card-title">Brand: ${phone.brand}</h5>
-                            <button onclick="PhoneInfo('${phone.slug}')" class="btn btn-success px-5 mt-2"> Details</button>
+                            <a  onclick="PhoneInfo('${phone.slug}')" class="btn btn-success px-5 mt-2" type ="button"> Details</a>
                         </div
                     </div> 
               
@@ -87,7 +85,7 @@ const iphnMiniInfo = (idInfomini) => {
     // sppiner
     document.getElementById('phn-info').innerHTML = "";
     document.getElementById('iphn-mini').innerHTML = `
-    <div class="card mb-3 w-50 mx-auto">
+    <div class="card mb-3 w-50 mx-auto" >
         <img src="${idInfomini.image}" class="card-img-top w-50 mx-auto p-4" alt="...">
         <div class="card-body">
             <h2> Phone Name : ${idInfomini.name} </h2>
@@ -99,14 +97,21 @@ const iphnMiniInfo = (idInfomini) => {
             <b>Memory:</b> ${idInfomini.mainFeatures.memory} <br>
             <b> Sensors:</b> ${idInfomini.mainFeatures.sensors}<br><br>
       
-        </p>     
+        </p>   
+        <h5> Others:</h5>
+            <p>
+                 <b>WALN</b>: Not Found  <br>
+                 <b>Bluetooth:</b> Not Found  <br>
+                <b>GPS:</b>Not Found  <br>
+                 <b>NFC:</b> Not Found  <br>
+                 <b>Radio: </b> Not Found <br>
+                     <b> USB :</b> Not Found  
+            </p>       
         
         </div>
 </div>
     
     `
-
-
 
 }
 const showPhoneInfo = (idInfo) => {
