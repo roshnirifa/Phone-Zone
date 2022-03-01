33c63bad061db532e88f7b46a5840a8f4c6c0a5f
roshnirifa <roshnirifa998@gmail.com>
2022-03-01 14:53:30 +0600

    responsive added

--- js/app.js
+++ js/app.js
@@ -11,39 +11,41 @@ const searchAllPhone = () => {
     if (searchFieldText == '') {
         alert('please write something to display')
     }
+
     else {
         // load data
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`
         fetch(url)
             .then(res => res.json())
-            .then(data => displaySerachResult(data.data))
+            .then(data => displaySerachResult(data.data.slice(0, 20)))
     }
 }
 
 const displaySerachResult = phones => {
     // console.log(phones);
     for (const phone of phones) {
-        console.log(phone);
+        // console.log(phone);
+
         const searchResult = document.getElementById('search-result');
         const div = document.createElement('div')
         div.classList.add('card')
         div.innerHTML = `
-            <div class="col">
-                    <div class="card  ">
-                        <img src="${phone.image}" class="card-img-top w-50 p-4" alt="...">
+                <div class=" col text-center m-auto bg-dark>
+                    <div class="card ">
+                        <img src="${phone.image}" class="card-img-top w-50 p-2" alt="...">
                         <div class="card-body">
-                        <h4> Phone Name : ${phone.phone_name} </h4>
-                        <h5 class="card-title">Brand: ${phone.brand}</h5>
-                        <button onclick="PhoneInfo('${phone.slug}')" class="btn btn-success"> Details</button>
+                            <h4> Phone Name : ${phone.phone_name} </h4>
+                            <h5 class="card-title">Brand: ${phone.brand}</h5>
+                            <button onclick="PhoneInfo('${phone.slug}')" class="btn btn-success px-5 mt-2"> Details</button>
                         </div
-                    </div >
-                </div >
-
+                    </div> 
+                </div>
     `;
         searchResult.appendChild(div);
 
     }
 }
+
 const PhoneInfo = (info) => {
     // console.log(info);
 
@@ -54,22 +56,26 @@ const PhoneInfo = (info) => {
 
 }
 const showPhoneInfo = (idInfo) => {
+
     // console.log(idInfo);
+
+
+
     document.getElementById('phn-info').innerHTML = `
                 <div class="card mb-3 w-50 mx-auto">
                     <img src="${idInfo.image}" class="card-img-top w-50 mx-auto p-4" alt="...">
                     <div class="card-body">
                         <h2> Phone Name : ${idInfo.name} </h2>
-                        <h3> Release Date : ${idInfo.releaseDate} </h3>
+                        <h4 id="releaseDate"> Release Date :${idInfo.releaseDate}</h4
                     <p>
-                        <h5>Main Features:</h5> <br>
+                        <h5>Main Features:</h5>
                             <b>Stroages:</b> ${idInfo.mainFeatures.storage} <br>
                              <b>Chip:</b> ${idInfo.mainFeatures.chipSet} <br>
                              <b>Memory:</b> ${idInfo.mainFeatures.memory} <br>
-                             <b> Sensors:</b> ${idInfo.mainFeatures.sensors}<br>
+                             <b> Sensors:</b> ${idInfo.mainFeatures.sensors}<br><br>
                    <h5> Others:</h5>
                     <p>
-                         <b>WALN</b>:  ${idInfo.others.WLAN} <br>
+                         <b>WALN</b>: ${idInfo.others.WLAN} <br>
                          <b>Bluetooth </b> ${idInfo.others.Bluetooth}<br>
                         <b>GPS: </b> ${idInfo.others.GPS}<br>
                          <b>NFC: </b> ${idInfo.others.NFC}<br>
@@ -79,5 +85,7 @@ const showPhoneInfo = (idInfo) => {
                     </div>
                 </div>
             
-    `
+    `;
+
+
 }
