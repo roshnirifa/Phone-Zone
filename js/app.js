e0ed0dc12f62ef0b9bc1806aebc72cfe284b9d43
roshnirifa <roshnirifa998@gmail.com>
2022-03-01 22:11:45 +0600

    iphon 13 mini added

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const searchAllPhone = () => {
     searchField.value = "";
     document.getElementById('search-result').innerHTML = '';
     document.getElementById('phn-info').innerHTML = '';
+    document.getElementById('iphn-mini').innerHTML = '';
+
 
 
     // search error
@@ -17,7 +19,14 @@ const searchAllPhone = () => {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`
         fetch(url)
             .then(res => res.json())
-            .then(data => displaySerachResult(data.data.slice(0, 20)))
+            .then(data => {
+                if (data.status === false) {
+                    alert('Nod data found');
+                }
+                else {
+                    displaySerachResult(data.data.slice(0, 20))
+                }
+            })
     }
 }
 
@@ -49,46 +58,78 @@ const displaySerachResult = phones => {
     }
 }
 
+
+// phone detailes
 const PhoneInfo = (info) => {
     // console.log(info);
+    if (info === "apple_iphone_13_mini-11104") {
+        const iphn13miniUrl = `https://openapi.programming-hero.com/api/phone/${info}`
+        fetch(iphn13miniUrl)
+            .then(res => res.json())
+            .then(data => iphnMiniInfo(data.data))
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phone/${info}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => showPhoneInfo(data.data))
+    }
 
-    const url = `https://openapi.programming-hero.com/api/phone/${info}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => showPhoneInfo(data.data))
+}
+// iphn13 mini data
+const iphnMiniInfo = (idInfomini) => {
+    // console.log(idInfomini);
+    document.getElementById('phn-info').innerHTML = "";
+    document.getElementById('iphn-mini').innerHTML = `
+    <div class="card mb-3 w-50 mx-auto">
+        <img src="${idInfomini.image}" class="card-img-top w-50 mx-auto p-4" alt="...">
+        <div class="card-body">
+            <h2> Phone Name : ${idInfomini.name} </h2>
+            <h4 id="releaseDate"> Release Date :${idInfomini.releaseDate ? `${idInfomini.releaseDate}` : `Not Found`}</h4>
+        <p>
+            <h5>Main Features:</h5>
+            <b>Stroages:</b> ${idInfomini.mainFeatures.storage} <br>
+            <b>Chip:</b> ${idInfomini.mainFeatures.chipSet} <br>
+            <b>Memory:</b> ${idInfomini.mainFeatures.memory} <br>
+            <b> Sensors:</b> ${idInfomini.mainFeatures.sensors}<br><br>
+      
+        </p>     
+        
+        </div>
+</div>
+    
+    `
+
+
 
 }
 const showPhoneInfo = (idInfo) => {
-
     // console.log(idInfo);
-
-
-
+    document.getElementById('iphn-mini').innerHTML = '';
     document.getElementById('phn-info').innerHTML = `
-                <div class="card mb-3 w-50 mx-auto">
-                    <img src="${idInfo.image}" class="card-img-top w-50 mx-auto p-4" alt="...">
-                    <div class="card-body">
-                        <h2> Phone Name : ${idInfo.name} </h2>
-                        <h4 id="releaseDate"> Release Date :${idInfo.releaseDate ? `${idInfo.releaseDate}` : `Not Found`}</h4>
-                    <p>
-                        <h5>Main Features:</h5>
-                            <b>Stroages:</b> ${idInfo.mainFeatures.storage} <br>
-                             <b>Chip:</b> ${idInfo.mainFeatures.chipSet} <br>
-                             <b>Memory:</b> ${idInfo.mainFeatures.memory} <br>
-                             <b> Sensors:</b> ${idInfo.mainFeatures.sensors}<br><br>
-                   <h5> Others:</h5>
-                    <p>
-                         <b>WALN</b>: ${idInfo.others.WLAN} <br>
-                         <b>Bluetooth </b> ${idInfo.others.Bluetooth}<br>
-                        <b>GPS: </b> ${idInfo.others.GPS}<br>
-                         <b>NFC: </b> ${idInfo.others.NFC}<br>
-                         <b>Radio: </b> ${idInfo.others.Radio}<br>
-                             <b> USB : </b> ${idInfo.others.USB} 
-                    </p>     
-                    </div>
-                </div>
-            
-    `;
-
+        <div class="card mb-3 w-50 mx-auto">
+            <img src="${idInfo.image}" class="card-img-top w-50 mx-auto p-4" alt="...">
+            <div class="card-body">
+                <h2> Phone Name : ${idInfo.name} </h2>
+                <h4 id="releaseDate"> Release Date :${idInfo.releaseDate ? `${idInfo.releaseDate}` : `Not Found`}</h4>
+            <p>
+                <h5>Main Features:</h5>
+                    <b>Stroages:</b> ${idInfo.mainFeatures.storage} <br>
+                     <b>Chip:</b> ${idInfo.mainFeatures.chipSet} <br>
+                     <b>Memory:</b> ${idInfo.mainFeatures.memory} <br>
+                     <b> Sensors:</b> ${idInfo.mainFeatures.sensors}<br><br>
+           <h5> Others:</h5>
+            <p>
+                 <b>WALN</b>: ${idInfo.others.WLAN} <br>
+                 <b>Bluetooth </b> ${idInfo.others.Bluetooth}<br>
+                <b>GPS: </b> ${idInfo.others.GPS}<br>
+                 <b>NFC: </b> ${idInfo.others.NFC}<br>
+                 <b>Radio: </b> ${idInfo.others.Radio}<br>
+                     <b> USB : </b> ${idInfo.others.USB} 
+            </p>     
+            </div>
+        </div>
+    
+`;
 
 }
